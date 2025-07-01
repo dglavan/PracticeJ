22e82b4f1a9106e920f2db764e677c7266409eb5
dglavan <dragosglavan91@gmail.com>
2025-07-01 17:54:22 +0300

    Practice problems part 2

--- home/src/SelectionSort.java
+++ home/src/SelectionSort.java
@@ -5,7 +5,7 @@ public class SelectionSort {
         for(int i=0;i<n-1;i++){
             int minIndex = i;
             for(int j=i+1;j<n;j++){
-                if(arr[j]>arr[minIndex]){
+                if(arr[j] < arr[minIndex]){
                     minIndex = j;
                 }
             }
